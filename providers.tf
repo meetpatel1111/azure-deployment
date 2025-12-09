6035be2ae2b22327712ca57f48565c0b96bcb4f6
Meetkumar Patel <pmeet464@gmail.com>
2025-12-09 19:19:41 +0530

    Added databricks provider

--- providers.tf
+++ providers.tf
@@ -1,12 +1,20 @@
 terraform {
-  required_version = ">= 1.4.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
     }
+    databricks = {
+      source  = "databricks/databricks"
+      version = "~> 1.40.0"
+    }
   }
 }
+
 provider "azurerm" {
   features {}
+}
+
+provider "databricks" {
+  host = module.databricks_workspace[0].workspace_url
 }
