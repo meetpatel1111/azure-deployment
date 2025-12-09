ea1dbc2fceeb153353ce24ea33ab8090ecd561f9
Meetkumar Patel <pmeet464@gmail.com>
2025-12-09 18:40:45 +0530

    Added public_network_access_enabled for storage account

--- modules/storage_account/main.tf
+++ modules/storage_account/main.tf
@@ -1,11 +1,10 @@
 resource "azurerm_storage_account" "this" {
-  name                     = var.name
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
-  min_tls_version          = "TLS1_2"
-  allow_blob_public_access = false
-
-  tags = var.tags
+  name                          = var.name
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
+  account_tier                  = var.account_tier
+  account_replication_type      = var.account_replication_type
+  min_tls_version               = "TLS1_2"
+  public_network_access_enabled = false
+  tags                          = var.tags
 }
