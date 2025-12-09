3c9b50466f3d1de583bf4cda8a1f593aa3f48412
Meetkumar Patel <pmeet464@gmail.com>
2025-12-09 22:12:55 +0530

    Removed managed_identity_principal_id

--- modules/databricks_workspace/outputs.tf
+++ modules/databricks_workspace/outputs.tf
@@ -9,7 +9,3 @@ output "workspace_id" {
 output "workspace_url" {
   value = azurerm_databricks_workspace.this.workspace_url
 }
-
-output "managed_identity_principal_id" {
-  value = azurerm_databricks_workspace.this.identity.principal_id
-}
