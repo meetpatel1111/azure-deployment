1bab1b9d3e5020980f93d5f07d3605477bba6cf1
Meetkumar Patel <pmeet464@gmail.com>
2025-12-09 18:34:15 +0530

    Fixed storage account and iam creation issue

--- main.tf
+++ main.tf
@@ -8,6 +8,67 @@ locals {
   nic_name    = "nic-${local.suffix}"
   vm_name     = "vm-${local.suffix}"
 
+  resource "azurerm_resource_group" "rg" {
+    name     = local.rg_name
+    location = var.location
+    tags     = var.tags
+  }
+
+  module "vnet" {
+    source              = "./modules/vnet"
+    name                = local.vnet_name
+    cidr                = local.vnet_cidr
+    location            = var.location
+    resource_group_name = azurerm_resource_group.rg.name
+    tags                = var.tags
+  }
+
+  module "subnet" {
+    source              = "./modules/subnet"
+    name                = local.subnet_name
+    cidr                = local.subnet_cidr
+    resource_group_name = azurerm_resource_group.rg.name
+    vnet_name           = module.vnet.vnet_name
+  }
+
+  module "nsg" {
+    source              = "./modules/nsg"
+    name                = local.nsg_name
+    location            = var.location
+    resource_group_name = azurerm_resource_group.rg.name
+    tags                = var.tags
+  }
+
+  module "pip" {
+    source              = "./modules/public_ip"
+    name                = local.pip_name
+    location            = var.location
+    resource_group_name = azurerm_resource_group.rg.name
+    tags                = var.tags
+  }
+
+  module "nic" {
+    source              = "./modules/nic"
+    name                = local.nic_name
+    location            = var.location
+    resource_group_name = azurerm_resource_group.rg.name
+    subnet_id           = module.subnet.subnet_id
+    public_ip_id        = module.pip.public_ip_id
+    tags                = var.tags
+  }
+
+  module "vm" {
+    source               = "./modules/vm"
+    name                 = local.vm_name
+    location             = var.location
+    resource_group_name  = azurerm_resource_group.rg.name
+    nic_id               = module.nic.nic_id
+    admin_username       = var.admin_username
+    admin_ssh_public_key = var.admin_ssh_public_key
+    vm_size              = var.vm_size
+    tags                 = var.tags
+  }
+
   # Storage account naming (no hyphens allowed)
   storage_account_name = "st${replace(local.suffix, "-", "")}"
   vnet_cidr            = "10.60.0.0/16"
@@ -30,65 +91,4 @@ module "iam_rg" {
   scope                = azurerm_resource_group.rg.id
   role_definition_name = var.iam_role_definition_name
   principal_id         = var.iam_principal_id
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = local.rg_name
-  location = var.location
-  tags     = var.tags
-}
-
-module "vnet" {
-  source              = "./modules/vnet"
-  name                = local.vnet_name
-  cidr                = local.vnet_cidr
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tags                = var.tags
-}
-
-module "subnet" {
-  source              = "./modules/subnet"
-  name                = local.subnet_name
-  cidr                = local.subnet_cidr
-  resource_group_name = azurerm_resource_group.rg.name
-  vnet_name           = module.vnet.vnet_name
-}
-
-module "nsg" {
-  source              = "./modules/nsg"
-  name                = local.nsg_name
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tags                = var.tags
-}
-
-module "pip" {
-  source              = "./modules/public_ip"
-  name                = local.pip_name
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tags                = var.tags
-}
-
-module "nic" {
-  source              = "./modules/nic"
-  name                = local.nic_name
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  subnet_id           = module.subnet.subnet_id
-  public_ip_id        = module.pip.public_ip_id
-  tags                = var.tags
-}
-
-module "vm" {
-  source               = "./modules/vm"
-  name                 = local.vm_name
-  location             = var.location
-  resource_group_name  = azurerm_resource_group.rg.name
-  nic_id               = module.nic.nic_id
-  admin_username       = var.admin_username
-  admin_ssh_public_key = var.admin_ssh_public_key
-  vm_size              = var.vm_size
-  tags                 = var.tags
 }
